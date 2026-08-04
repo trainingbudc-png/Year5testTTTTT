1179fba6acb2ff6b9bd8fd3d88788d4a0abcf9e2
trainingbudc-png <training.budc@cpird.in.th>
2026-08-04 14:01:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@
 const CONFIG = {
     // 🌐 URL ของ Google Apps Script (ตัวใหม่ที่รวมโค้ดแล้ว)
     // สามารถใช้ลิงก์เดียวกันได้เลย เพราะระบบ Backend แยกส่วนให้แล้ว
-    WEB_APP_API: "https://script.google.com/macros/s/AKxxx/exec",
+    WEB_APP_API: "https://script.google.com/macros/s/AKfycbzQbCm2eHPDcKN28vYR1SCKgJ9_yfaLEk1Zw31oLCtxjVjl_VXw_6f3a504ufdKyg_0/exec",
 
     // 📱 LIFF IDs
-    LIFF_ID_CHECKIN: "20xxx-xxx",
-    LIFF_ID_HISTORY: "20xxx-xxx",
-    LIFF_ID_REGISTER: "20xxx-xxx",
+    LIFF_ID_CHECKIN: "2010861550-IFjnkOsj",
+    LIFF_ID_HISTORY: "2010861550-wenDuHW4",
+    LIFF_ID_REGISTER: "2010861550-knzIvbdB",
 
     // 📍 การตั้งค่าพิกัดสถานที่ (ละติจูด, ลองจิจูด)
     TARGET_LATITUDE: 16.807157,
